e32fd995bb66aec6457a422116e774132d7f4943
manvi <manvi@oniondev.com>
2025-02-24 21:01:12 +0530

    Fix : Removed extra files and urls

--- src/services&apis/config.js
+++ src/services&apis/config.js
@@ -3,8 +3,6 @@ const config = {
   api_url: "https://geoserver.core-stack.org",
   image_layer_url: "https://geoserver.core-stack.org:8443/geoserver/wms",
   image_layer_server_type: "geoserver",
-  UserPoolId: "us-east-1_WMarDvCqE",
-  ClientId: "4u3v5u5jfcq0kgjcsqf5jv0kgl",
   baseURL: "https://dbaf-2001-df4-e000-3fc3-b9f4-158d-b3d5-82e5.ngrok-free.app",
 };
 
--- src/services&apis/userpool.js
+++
@@ -1,6 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-const poolData = {
-  UserPoolId: "ap-south-1_vxlrNp9Sx",
-  ClientId: "2qsuinmt56almqrq94jkodum87",
-};
-export default new CognitoUserPool(poolData);
